757aec4525626d4a2499679e3dcda2c0d7decbdb
Tshiamo Justice Mothokho <rafxjay@gmail.com>
2026-08-28 05:15:25 +0200

    removed api folder. too any types and uncertaincies and hidden potential bug

--- diskicentral/api/models/UpdateResultDto.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export type UpdateResultDto = {
-    homeScore?: number | string | null;
-    awayScore?: number | string | null;
-};
-
